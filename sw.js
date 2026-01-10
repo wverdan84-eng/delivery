bb25032ccd13b20990d4da2492d376ad53bae43a
wverdan84-eng <wverdan84@gmail.com>
2026-01-10 11:23:40 -0300

    Criar sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'loja-pwa-v1';
+
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request).then(fetchRes => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(e.request, fetchRes.clone());
+          return fetchRes;
+        });
+      }).catch(() => caches.match('/index.html'));
+    })
+  );
+});
